b510943af16630cc45df7d3973ef5b1c7a0a12a8
Eugene Ware <eugene@noblesamurai.com>
2013-09-25 22:42:54 +1000

    batch with rollback

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
+var after = require('after'),
+    unique = require('lodash.uniq');
+
 module.exports = tx;
 function tx(db) {
   db.txPut = db.txPut || txPut.bind(db);
+  db.txBatch = db.txBatch || txBatch.bind(db);
   return db;
+}
+
+function txBatch(batch, opts, cb) {
+  if (typeof opts === 'function') {
+    cb = opts;
+    opts = undefined;
+  }
+
+  var db = this;
+  var args = [].slice.call(arguments);
+
+  var rollbackBatch = [];
+
+  var keys = unique(batch.map(function (op) {
+    return op.key;
+  }));
+
+  var next = after(keys.length, _batch);
+  keys.forEach(function (key) {
+    db.get(key, function (err, value) {
+      var type = 'put';
+      if (err) {
+        if (err.type !== 'NotFoundError') {
+          return next(err);
+        } else {
+          type = 'del';
+        }
+      }
+      rollbackBatch.push({ type: type, key: key, value: value });
+      next();
+    });
+  });
+
+  function _batch() {
+    return db.batch.call(db, batch, opts, function (err) {
+      if (err) return cb(err);
+      return cb(null, {
+        commit: function (_cb) {
+          return _cb(null);
+        },
+        rollback: function (_cb) {
+          db.batch(rollbackBatch, _cb);
+        }
+      });
+    });
+  }
 }
 
 function txPut(key, value, opts, cb) {
--- test/index.js
+++ test/index.js
@@ -2,6 +2,8 @@ var expect = require('expect.js'),
     path = require('path'),
     rimraf = require('rimraf'),
     level = require('level'),
+    range = require('range'),
+    after = require('after'),
     tx = require('..');
 
 describe('level-transaction', function() {
@@ -46,6 +48,67 @@ describe('level-transaction', function() {
         expect(err.type).to.equal('NotFoundError');
         done();
       });
+    }
+  });
+
+  it('should be able to run a transactional batch', function(done) {
+    var batch = range(0, 10).map(function (i) {
+      return {
+        type: 'put',
+        key: 'key ' + i,
+        value: 'value ' + i
+      };
+    });
+
+    db = tx(db);
+    db.txBatch(batch, function (err, tx) {
+      if (err) return done(err);
+      tx.commit(get);
+    });
+
+    function get(err) {
+      if (err) return done(err);
+      db.get('key 7', function (err, value) {
+        expect(value).to.equal('value 7');
+        done();
+      });
+    }
+  });
+
+  it('should be able to run a transactional batch with rollback', function(done) {
+    var batch = range(0, 10).map(function (i) {
+      return {
+        type: 'put',
+        key: 'key ' + i,
+        value: 'value ' + i
+      };
+    });
+
+    db = tx(db);
+    db.txBatch(batch, function (err, tx) {
+      if (err) return done(err);
+      tx.rollback(get);
+    });
+
+    var count = 0;
+    function get(err) {
+      if (err) return done(err);
+      var keys = batch.map(function (op) {
+        return op.key;
+      });
+      var next = after(keys.length, check);
+      keys.forEach(function (key) {
+        db.get(key, function (err, value) {
+          expect(err.type).to.equal('NotFoundError');
+          count++;
+          next();
+        });
+      });
+    }
+
+    function check() {
+      expect(count).to.equal(batch.length);
+      done();
     }
   });
 });
